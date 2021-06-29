1fa36dfbc0bc84668aad6ecde2a50e43c155aaf5
Subhajit Sahu <wolfram77@gmail.com>
2021-06-29 11:44:15 +0530

    :skunk: w/ BitSetUnsorted

--- src/BitSetUnsorted.hxx
+++ src/BitSetUnsorted.hxx
@@ -1,60 +1,127 @@
 #pragma once
-#include "_main.hxx"
 #include <utility>
 #include <vector>
+#include <iterator>
 #include <algorithm>
+#include "_main.hxx"
 
+using std::forward_iterator_tag;
 using std::pair;
 using std::vector;
-using std::iter_swap;
 using std::find_if;
+using std::swap;
 
 
 
 
 template <class T=NONE>
-class BitSetUnsorted {
-  vector<pair<int, T>> ids;
+class BitsetUnsorted {
+  vector<uint16_t> highs;
+  vector2d<pair<uint16_t, T>> lows;
+  int N;
 
-  // Cute helpers
+
+  // Cute helpers.
   private:
-  auto lookup(int id) const {
-    auto fn = [&](const auto& e) { return e.first == id; };
-    return find_if(ids.begin(), ids.end(), fn);
+  static uint16_t hi(int id) { return id >> 16; }
+  static uint16_t lo(int id) { return id & 0xFF; }
+  static int full(uint16_t hi, uint16_t lo) { return (hi<<16) | lo; }
+
+  protected:
+  pair<int, int> lookup(int id) const {
+    int i = findEqIndex(highs, hi(id));
+    int j = i<0? -1 : findIfEqIndex(lows, [&](const auto& e) { return e.first==lo(id); });
+    return {i, j};
   }
+
+
+  // Get values.
+  public:
+  class Iterator {
+    using iterator = Iterator;
+    public:
+    int i, j;
+
+    public:
+    using iterator_category = forward_iterator_tag;
+    using difference_type   = int;
+    using value_type = pair<int, T>;
+    using reference  = value_type;
+    using pointer    = const value_type*;
+
+    public:
+    Iterator(int i, int j) : i(i), j(j) {}
+    reference operator*() const {
+      const auto& hi = highs[i];
+      const auto& lo = lows[i][j];
+      return make_pair(full(hi, lo.first), lo.second);
+    }
+    iterator& operator++() {
+      if (lows[i].size() >= ++j) { ++i; j = 0; }
+      return *this;
+    }
+    iterator operator++(int) {
+      iterator it = *this; ++(*this);
+      return it;
+    }
+    friend bool operator==(const iterator& l, const iterator& r) {
+      return l.i==r.i && l.j==r.j;
+    }
+    friend bool operator!=(const iterator& l, const iterator& r) {
+      return l.i!=r.i && l.j!=r.j;
+    }
+  };
+
 
   // Read as iterable.
   public:
-  auto entries() const { return transform(ids, [](const auto& e) { return e; }); }
-  auto keys()    const { return transform(ids, [](const auto& e) { return e.first; }); }
-  auto values()  const { return transform(ids, [](const auto& e) { return e.second; }); }
+  auto entries() const {
+    auto b = Iterator(0, 0);
+    auto e = Iterator(highs.size(), 0);
+    return makeIter(b, e);
+  }
+  auto keys()    const { return transform(entries(), [](const auto& e) { return e.first; }); }
+  auto values()  const { return transform(entries(), [](const auto& e) { return e.second; }); }
+
 
   // Read operations.
   public:
-  size_t size()      const { return ids.size(); }
-  bool   has(int id) const { return lookup(id) != ids.end(); }
-  T      get(int id) const { auto it = lookup(id); return it == ids.end()? T() : (*it).second; }
+  size_t size() const { return N; }
+
+  bool has(int id) const {
+    auto [i, j] = lookup(id);
+    return i>=0 && j>=0;
+  }
+
+  T get(int id) const {
+    auto [i, j] = lookup(id);
+    return i<0 || j<0? T() : lows[i][j].second;
+  }
 
   // Write operations
   public:
   void clear() {
-    ids.clear();
+    highs.clear();
+    lows.clear();
   }
 
   void set(int id, T v) {
-    auto it = lookup(id);
-    if (it == ids.end()) return;
-    (*it).second = v;
+    auto [i, j] = lookup(id);
+    if (i<0 || j<0) return;
+    lows[i][j].second = v;
   }
 
   void add(int id, T v=T()) {
-    if (!has(id)) ids.push_back({id, v});
+    auto [i, j] = lookup(id);
+    if (i>=0 && j>=0) return;
+    if (i<0) highs.push_back(hi(id));
+    lows.push_back({lo(id), v});
   }
 
   void remove(int id) {
-    auto it = lookup(id);
-    if (it == ids.end()) return;
-    iter_swap(it, ids.end()-1);
-    ids.pop_back();
+    auto [i, j] = lookup(id);
+    if (i<0 || j<0) return;
+    swap(lows[i].back(), lows[i][j]);
+    lows[i].pop_back();
   }
 };
